2dbaf3597e2b68ef09673538e8dd72914d089373
lat165 <lat165@yahoo.com>
2013-12-23 20:33:53 -0500

    project with custom theme

--- Earsy/gen/com/rockwood/earsy/R.java
+++ Earsy/gen/com/rockwood/earsy/R.java
@@ -22,15 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int play_button=0x7f020001;
+        public static final int ic_playbutton=0x7f020001;
     }
     public static final class id {
+        public static final int act_pitch_layout=0x7f080001;
         public static final int action_settings=0x7f080005;
         public static final int button=0x7f080000;
         public static final int button1=0x7f080004;
-        public static final int tableLayout1=0x7f080001;
-        public static final int tableRow1=0x7f080002;
-        public static final int textView1=0x7f080003;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -44,22 +44,13 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int main_btnStartTraining=0x7f050002;
-        public static final int play_btnContent=0x7f050004;
+        public static final int pitch_test_instruc=0x7f050004;
+        public static final int play_btnContent=0x7f050005;
         public static final int title_activity_pitch_test=0x7f050003;
-        public static final int title_section2=0x7f050005;
         public static final int title_section3=0x7f050006;
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -71,10 +62,27 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int AppBaseTheme=0x7f060002;
+        public static final int CustomActionBarOrange=0x7f060001;
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+    <style name="AppBaseTheme" parent="android:Theme.Light">
+        
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+    </style>
+
+     Application theme. 
+    <style name="AppTheme" parent="AppBaseTheme">
+         All customizations that are NOT specific to a particular API-level can go here.
+    </style>
+    
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int CustomThemeOrange=0x7f060000;
     }
 }
