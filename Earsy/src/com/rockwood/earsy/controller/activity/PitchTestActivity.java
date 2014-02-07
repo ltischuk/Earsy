8dc32589d6fbcbe0a8016a6ea6a9761a354dae6a
lat165 <lat165@yahoo.com>
2014-02-06 22:18:44 -0500

    added the fragment dialog for when correct answer is chosen

--- Earsy/src/com/rockwood/earsy/controller/activity/PitchTestActivity.java
+++ Earsy/src/com/rockwood/earsy/controller/activity/PitchTestActivity.java
@@ -1,16 +1,9 @@
 package com.rockwood.earsy.controller.activity;
 
-import com.rockwood.earsy.R;
-import com.rockwood.earsy.model.MusicNote;
-import com.rockwood.earsy.model.PitchTest;
-import com.rockwood.earsy.utils.Utils;
-import com.rockwood.earsy.view.AnswerDialogFragment;
-import com.rockwood.earsy.view.PianoView;
-
+import android.app.Activity;
+import android.app.DialogFragment;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,75 +11,99 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.rockwood.earsy.R;
+import com.rockwood.earsy.model.MusicNote;
+import com.rockwood.earsy.model.PitchTest;
+import com.rockwood.earsy.utils.Utils;
+import com.rockwood.earsy.view.AnswerDialogFragment;
+import com.rockwood.earsy.view.PianoView;
 
-public class PitchTestActivity extends Activity {
-    
-    private PitchTest test;
-    private MediaPlayer mp;
-    private TextView textViewQNum;
-    private AnswerDialogFragment dialog;
-    
+public class PitchTestActivity extends Activity
+{
+
+	private PitchTest test;
+	private MediaPlayer mp;
+	private TextView textViewQNum;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);		
+	protected void onCreate(final Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pitch_test);
-		
+
 		test = new PitchTest();
-		mp = MediaPlayer.create(this, Utils.getMp3FromMusicNote(test.getNoteToPlay()));
+		mp = MediaPlayer.create(this,
+				Utils.getMp3FromMusicNote(test.getNoteToPlay()));
 		// Get our TextView object.
-		textViewQNum = (TextView)findViewById(R.id.textViewQNum);
-		textViewQNum.setText(test.getQuestionNumberInfo());	
-		
+		textViewQNum = (TextView) findViewById(R.id.textViewQNum);
+		textViewQNum.setText(test.getQuestionNumberInfo());
+
 		final ImageButton playButton = (ImageButton) findViewById(R.id.playButton);
-		playButton.setOnTouchListener(new View.OnTouchListener() {
-		    
-		    @Override
-		    public boolean onTouch(View v, MotionEvent event) {
-			if (event.getAction() == MotionEvent.ACTION_DOWN ) 
+		playButton.setOnTouchListener(new View.OnTouchListener()
+		{
+
+			@Override
+			public boolean onTouch(final View v, final MotionEvent event)
 			{
-				mp.start();
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					mp.start();
+				}
+				return true;
 			}
-			return true;
-		    }
 		});
-		
+
 		final View pianoView = findViewById(R.id.pianoView);
-		pianoView.setOnTouchListener(new View.OnTouchListener() {
-		    
-		    @Override
-		    public boolean onTouch(View v, MotionEvent event) {
-			checkAnswer(((PianoView) v).getNoteTouched( event.getX(),event.getY()));
-			return true;
-		    }
-		});			
-		
-		dialog = new AnswerDialogFragment();
+		pianoView.setOnTouchListener(new View.OnTouchListener()
+		{
+
+			@Override
+			public boolean onTouch(final View v, final MotionEvent event)
+			{
+				checkAnswer(((PianoView) v).getNoteTouched(event.getX(),
+						event.getY()));
+				return true;
+			}
+		});
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(final Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.pitch_test, menu);
 		return true;
 	}
-	
-	public void checkAnswer(MusicNote guessedNote)
+
+	public void checkAnswer(final MusicNote guessedNote)
 	{
-	    if(test.guessNote(guessedNote))
-	    {
-	    	dialog.show(getFragmentManager(), "Result");
-	    }
-	    else
-	    {
-	    	Toast.makeText(this, Utils.WRONGTOAST, Toast.LENGTH_SHORT).show();
-	    }
+		if (test.guessNote(guessedNote)) {
+			showDialog();
+		} else {
+			Toast.makeText(getApplicationContext(), R.string.wrong_answer, Toast.LENGTH_SHORT).show();
+		}
 	}
-	
+
 	public void setNextQuestion()
 	{
 		test.incQuestionNum();
 		textViewQNum.setText(test.getQuestionNumberInfo());
 	}
+	
+	void showDialog() {
+	    DialogFragment newFragment = AnswerDialogFragment.newInstance(
+	            R.string.result);
+	    newFragment.show(getFragmentManager(), "dialog");
+	}
 
+	public void doPositiveClick() {
+	    // Do stuff here.
+	    
+	}
 
+	public void doNegativeClick() {
+	    // Do stuff here.
+	   
+	}
+
+	
 }
--- Earsy/src/com/rockwood/earsy/view/AnswerDialogFragment.java
+++ Earsy/src/com/rockwood/earsy/view/AnswerDialogFragment.java
@@ -1,36 +1,71 @@
 package com.rockwood.earsy.view;
 
-import com.rockwood.earsy.R;
-import com.rockwood.earsy.controller.activity.PitchTestActivity;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
-public class AnswerDialogFragment extends DialogFragment
+import com.rockwood.earsy.R;
+import com.rockwood.earsy.controller.activity.PitchTestActivity;
+
+public final class AnswerDialogFragment extends DialogFragment
 {
+	public static AnswerDialogFragment newInstance(final int title)
+	{
+		final AnswerDialogFragment frag = new AnswerDialogFragment();
+		final Bundle args = new Bundle();
+		args.putInt("title", title);
+		frag.setArguments(args);
+		return frag;
+	}
+
+	@Override
+	public Dialog onCreateDialog(final Bundle savedInstanceState)
+	{
+		// Use the Builder class for convenient dialog construction
+		final AlertDialog.Builder builder = new AlertDialog.Builder(
+				getActivity());
+		final PitchTestActivity pitchTest = (PitchTestActivity) getActivity();
+		builder.setMessage(R.string.correct_answer)
+				.setPositiveButton(R.string.next_question,
+						new DialogInterface.OnClickListener()
+						{
+							@Override
+							public void onClick(final DialogInterface dialog,
+									final int id)
+							{
+								pitchTest.setNextQuestion();
+							}
+						})
+				.setNegativeButton(R.string.quit,
+						new DialogInterface.OnClickListener()
+						{
+							@Override
+							public void onClick(final DialogInterface dialog,
+									final int id)
+							{
+								// User cancelled the dialog
+							}
+						});
+		// Create the AlertDialog object and return it
+		return builder.create();
+	}
 	
-	    @Override
-	    public Dialog onCreateDialog(Bundle savedInstanceState) {
-	        // Use the Builder class for convenient dialog construction
-	        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-	        final PitchTestActivity pitchTest = (PitchTestActivity) getActivity();
-	        builder.setMessage(R.string.correct_answer)
-	               .setPositiveButton(R.string.next_question, new DialogInterface.OnClickListener() {
-	                   public void onClick(DialogInterface dialog, int id) {
-	                	   pitchTest.setNextQuestion();
-	                   }
-	               })
-	               .setNegativeButton(R.string.quit, new DialogInterface.OnClickListener() {
-	                   public void onClick(DialogInterface dialog, int id) {
-	                       // User cancelled the dialog
-	                   }
-	               });
-	        // Create the AlertDialog object and return it
-	        return builder.create();
-	    }
-	
+	void showDialog() {
+	    DialogFragment newFragment = AnswerDialogFragment.newInstance(
+	            R.string.result);
+	    newFragment.show(getFragmentManager(), "dialog");
+	}
+
+	public void doPositiveClick() {
+	    // Do stuff here.
+	    
+	}
+
+	public void doNegativeClick() {
+	    // Do stuff here.
+	   
+	}
+
 }
